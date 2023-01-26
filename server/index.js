1451d4eb18674cf17a227f00c2345c4feab7b208
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-01-26 14:39:19 +0530

    file renaming

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = 5000;
 
-const connectDB = require("../config/db.js");
+const connectDB = require("./config/db.js");
 connectDB();
 
 app.use(express.static("public"));
@@ -13,10 +13,8 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 // Routes
-app.use("/api/files", require("../routes/files"));
-app.use("/files", require("../routes/show"));
-app.use("/files/download", require("../routes/download"));
+app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
 
 app.listen(PORT, () => console.log(`server running on PORT:${PORT}`));
-
-module.exports = app;
